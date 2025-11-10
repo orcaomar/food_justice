7c23230b1a732f7eba1a2685a2a66307b5df6222
Omar Khan <110122+orcaomar@users.noreply.github.com>
2025-11-09 19:59:56 -0500

    local background video

--- food-justice-research/src/components/Hero.jsx
+++ food-justice-research/src/components/Hero.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Hero.css';
+import backgroundVideo from '../assets/homepage/background_video.mp4';
 
 const Hero = () => {
   return (
     <div className="hero">
-      <video src="https://framerusercontent.com/assets/RsBoFqSaDSL4WwhPHMXwsYGfzw.mp4" loop muted playsInline autoPlay />
+      <video src={backgroundVideo} loop muted playsInline autoPlay />
       <div className="hero-text">
         <h1>Flemingdon & Thorncliffe Food Justice</h1>
         <a href="https://youtu.be/h1124yB9sc0?si=Zu4JN6y5PCmsJX5g" className="video-button">See Video</a>
