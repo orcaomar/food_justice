5091571984cc88d8939666f7e7c03ce4013d54a5
Omar Khan <110122+orcaomar@users.noreply.github.com>
2025-11-11 12:36:47 -0500

    audio

--- food-justice-research/src/data/IncreasingFoodInsecurityData.js
+++ food-justice-research/src/data/IncreasingFoodInsecurityData.js
@@ -11,6 +11,14 @@ import foodBank from '../assets/challenges/sub_pages/iUKFDgtehToaeU7EumG7q83L9r0
 import supplyDemand from '../assets/challenges/sub_pages/LE7GEHXcWeliI7TQkDKr7Zscdu4.jpg';
 
 import demandsAudio from '../assets/audio/Tara-Omar-maryam-Workers.mp3';
+import pandemicAudio from '../assets/audio/Maryam-Worker.mp3';
+import moreThanFoodAudio from '../assets/audio/Maha-Resident.mp3';
+import hiddenTollsAudio from '../assets/audio/Sonia-resident.mp3';
+import meetingDemandsAudio from '../assets/audio/Maryam-Worker (1).mp3';
+import complexityAudio from '../assets/audio/Aaliyah-worker.mp3';
+import childrensHealthAudio from '../assets/audio/Farah-resident.mp3';
+import foodBankAudio from '../assets/audio/naila-worker (1).mp3';
+import supplyDemandAudio from '../assets/audio/Maryam-worker (2).mp3';
 
 export const increasingFoodInsecurityData = {
   title: 'Increasing and Deepening Food Insecurity',
@@ -52,57 +60,78 @@ So every week we get new clients. We also get some people that aren't scheduled 
       title: 'The Pandemic Effects',
       text: 'The COVID-19 pandemic exacerbated the demand for emergency food distribution, and community agencies and grassroots initiatives struggled to respond.',
       image: pandemicEffects,
-      audio: 'placeholder.mp3',
-      transcript: 'Placeholder transcript',
+      audio: pandemicAudio,
+      transcript: `Maryam: I think the challenges, as Aida mentioned and Aaliyah mentioned and Tara was hinting at and Elise was ... We all went through those … challenges similar to that, before COVID. So the appointment system for the food bank came specifically during COVID because that was important. And we also tried to exercise that because we wanted to keep the numbers [manageable] to a certain extent. And this is why we didn't even send out flyers, because we knew that once you put a flyer out, X amount of people will turn up and then you have a limited supply of food as well. But then as you see people walking in, every time it would be 10 or 12 or 15 people just walking in just trying to find out like, "Oh, all right, this is what's happening over here and this is what's happening over here." So we used to keep some of the food share boxes on the side because we knew it would be four or five or 10 people walking in…. And at that point in time, it was sort of an emergency, so we wanted to be prepared at least for the walkins as well. But then again, you have a limited supply of food to give out as well. That is why one of the [challenges is] duplication, and everyone has hinted on that duplication. We try to compare notes with the agencies as well. Okay, how many people are using? How many people are using? Since we were working so closely together, we were able to really appreciate [that] yes, it's happening. People are going to the food bank. People are coming to the food distribution and then they are also crossing the boundaries, going elsewhere as well. The same things were being told to us by the food bank, Flemingdon Food Bank. They know for sure [that] people have registered at the Flemingdon Food Bank. They're also registered elsewhere as well. So this has been happening. [Worker]`,
     },
     {
       title: 'More Than Food',
       text: 'We heard over and over again, that food insecurity is about so much more than food. The complexity of the challenges people live with can feel overwhelming for both workers and residents.',
       image: moreThanFood,
-      audio: 'placeholder.mp3',
-      transcript: 'Placeholder transcript',
+      audio: moreThanFoodAudio,
+      transcript: `Maha: I am a single daughter. I'm the only child. My mom raised me as a single mother. So I have to support her, no matter what. She is in her 70s. And she has a lot of health issues. She's alone. And so let's say if I want to bring her here, I need to show the government that I have so much money in my account. Only then will they allow her to come to Canada. They won't think that, “Oh, if the mother comes here, maybe she can leave the children at home and take even the odd jobs.” Right now, this restaurant was offering me a manager's post and I have to close the store, because I studied Culinary Arts, but I cannot leave my children at home and close at 12 o'clock in the night and come home at one.That will be completely unfair to my children.
+
+So, I have to support my mom. My son has mental health issues. So, there's other stuff going on that I have to take care of. The neighborhood, sometimes I feel is not safe, I love the neighborhood. I love this neighborhood. But some people there... It's everywhere, crime and violence, but there is an excess of crime going on in my area. And I feel a lot of pressure with finances, $200 is going back home. And then like another resident said, Ontario Works is not going up. Rent is going up. Food prices are going up. And this is like a puzzle box. [Resident]` ,
     },
     {
       title: 'Hidden Tolls',
       text: 'Food insecurity takes a toll on physical health and mental health.',
       image: hiddenTolls,
-      audio: 'placeholder.mp3',
-      transcript: 'Placeholder transcript',
+      audio: hiddenTollsAudio,
+      transcript: `Sonia:  
+
+You feel like you can't provide for your family, because of food insecurity. It does take a toll on your mental health. Like if you can't afford to fend for your family, it is depressing, right? [Resident]`,
     },
     {
       title: 'Meeting Demands',
       text: 'For workers, the challenges of meeting increasing demands are multiple, and sometimes the most urgent need is difficult to ascertain.',
       image: meetingDemands,
-      audio: 'placeholder.mp3',
-      transcript: 'Placeholder transcript',
+      audio: meetingDemandsAudio,
+      transcript: `Maryam: One more thing I wanted to point out more is that with all these operations that are happening in the community, we are not reaching even half of the population. We need to make that extra effort to find out other folks that are in need, but are not accessing it. Why are they not able to access [help]? We need to find out those barriers. Whether it is: “How can I present myself at the food bank?” “I have language barriers.” “I have transport issues.” This is important because people are still food insecure in the community. [Worker]`,
     },
     {
       title: 'Complexity',
       text: 'Workers recognize the complexity and depth of the crisis for families. Workers observed that people use the food bank for more than food. For some, the food bank is a gathering place, helping to alleviate isolation.',
       image: complexity,
-      audio: 'placeholder.mp3',
-      transcript: 'Placeholder transcript',
+      audio: complexityAudio,
+      transcript: `Aaliyah: And that's important because people make ties to the food bank. I remember sitting in a meeting with a bunch of different food banks and everyone was praising themselves that they have clients that have been coming for 20 years. [Multiple voices expressing concern, such as: "Oh no!"] and they're so loyal. I was losing my mind because I'm like, this is not a good thing! And more times, it's that they make connections. It's like, you're lining in the lineup every day, you see this person, "My mom has friends at the food bank and she just wants to go just to see them." That's why when organizations are offering other services, it can create that transition. So even if they have a job now or if they are getting income or they don't really need the food anymore, they still want to come. And that happened a lot at the food banks that I would visit. And they're like, "No, I totally come and really see my friend. But while I'm here, I might as well just get that food," because all they offer is the food support. [Worker]`,
     },
     {
       title: "Children's Health",
       text: 'Food insecurity is especially challenging when trying to provide for children. Access to food affects physical, social and emotional health.',
       image: childrensHealth,
-      audio: 'placeholder.mp3',
-      transcript: 'Placeholder transcript',
+      audio: childrensHealthAudio,
+      transcript: `Farah: 
+
+Yeah, because when you go into the kitchen and you want to cook something, and you are missing something. And the kids want those things. You can't afford them. And you are spending money on oil, salt, basic things. But they want the other things. You cannot afford any oysters. As a mother, you can see your child is crying, and you can't afford it. I grow those tears. I'm still in this, but now I tell myself, it'll be okay. But it is this, because I manage to cut food for ourselves, but I have to buy for our kids. 
+
+Luann (Host): 
+Right. It is so much harder on the children.
+
+Farah: 
+Yeah. [Resident]`,
     },
     {
       title: 'Food Bank Produce vs Cans',
       text: 'Workers described the multiple challenges they face every day in trying to address food insecurity. For example, most food banks rely on donated food items, and fresh produce, meat, milk and eggs are always in short supply. And sometimes there is an abundance of food items that people won’t use.',
       image: foodBank,
-      audio: 'placeholder.mp3',
-      transcript: 'Placeholder transcript',
+      audio: foodBankAudio,
+      transcript: `Naila: 
+
+I wish, again, that people could have more fresh produce, instead of canned, because most of the time, people separate the cans, and leave the cans. That is not useful for the family. And we need more meat and protein. Milk, eggs – it’s not enough. And the Flemingdon Food Bank is doing a great job serving more than sometimes 300 people. Speed delivery or donations we're receiving, is not enough for families. I'm just worried. I know so many families, they don't have enough food on their table. That's sad. Six eggs is not enough for one week. Six small eggs. [Worker]
+
+Aida: 
+
+Demand is high, but you just have limited finances to only serve maybe 200 kids, 200 packs. But in terms of food banks, ... I don't know what the situation was like before, but I know they have too much stuff, which... Even in our groups, they were saying, "Anyone need this one? I would like to give this to someone, but in exchange, maybe you can give me something else." So people started even using WhatsApp groups and exchanging those unwanted products from the bank, for something they could really use. [Worker]`,
     },
     {
       title: 'Supply & Demand',
       text: 'Workers and residents both expressed concern that the mismatch between food needs and food received – in both quantity and cultural preferences – sometimes results in food waste.',
       image: supplyDemand,
-      audio: 'placeholder.mp3',
-      transcript: 'Placeholder transcript',
+      audio: supplyDemandAudio,
+      transcript: `Maryam: 
+
+The other thing, when we started, TNO was preparing those huge, massive 50 kilogram boxes, or something like that. That was a huge box. And it had all sorts of stuff in it. And this box was also going to the seniors, right? Senior buildings. And they were receiving that box every week. And then two or three or four times when they went to see what's happening, they would see those boxes lying one on top of each other, right? So the other thing we also found, which is very unfortunate, this is a waste of food. People would take the box, they would dump the stuff, they would take things out of it and the rest of the box was found near the dumpster. So these were the things. And on top of it, if you have dietary restrictions, there's lots of sugar boxes, cookie boxes and all sorts of things, not culturally appropriate or maybe even diet appropriate. And [some of the food is] sort of getting wasted, right? So these were the learnings and challenges that we were seeing the whole time. Then you learn on the go. [Worker]
+`,
     },
   ],
 };
--- food-justice-research/src/data/UnaffordabilityData.js
+++ food-justice-research/src/data/UnaffordabilityData.js
@@ -7,6 +7,13 @@ import childcareDemands from '../assets/challenges/sub_pages/h5YkQ3OeFZvCSc67haI
 import communityAwareness from '../assets/challenges/sub_pages/Nk3myKhzH0htrFNZA39uq4dzf4.jpg';
 import accessToOtherSupports from '../assets/challenges/sub_pages/D4NsYSKqRhOnMNTQaVwtP5vjFg.jpg';
 
+import canadianDreamAudio from '../assets/audio/Nadia-Alizah-residents.mp3';
+import risingCostsAudio from '../assets/audio/Alizah-Nadia-Maliha-residents.mp3';
+import housingCrisisAudio from '../assets/audio/Bilal-sonia-farah-ahmed-residents.mp3';
+import childcareDemandsAudio from '../assets/audio/Maliha-unaffordability_trimmed.mp3';
+import communityAwarenessAudio from '../assets/audio/Luannhost-Maliha-resident(2).mp3';
+import accessToOtherSupportsAudio from '../assets/audio/Aida-omarhost-Worker.mp3'
+
 const unaffordabilityData = {
   title: 'Unaffordability',
   image: unaffordability,
@@ -16,43 +23,174 @@ const unaffordabilityData = {
       title: 'Canadian "Dream"',
       text: 'We heard over and over that life in Canada is difficult. Some residents expressed surprise and disappointment in the everyday challenge they face in providing their families in Canada.',
       image: canadianDream,
-      audio: 'placeholder.mp3',
-      transcript: 'Placeholder transcript',
+      audio: canadianDreamAudio,
+      transcript: `Nadia: 
+
+Sometimes I feel like, our relatives back home, they're richer than here.
+
+Alizah: 
+
+Exactly! They have survived, they have relatives, support.
+
+Nadia: 
+
+This is Canadian-style poverty!
+
+Alizah: 
+
+Exactly. We are taking care of our kids.... and there is no support. [Residents]`,
     },
     {
       title: 'Rising Costs',
       text: 'A devastating reality for families is high rent, even for small and unsuitable housing. Increased food prices, especially since the pandemic, in combination with steadily inadequate family income.',
       image: risingCosts,
-      audio: 'placeholder.mp3',
-      transcript: 'Placeholder transcript',
+      audio: risingCostsAudio,
+      transcript: `Alizah: 
+
+Yeah. And I would like to add some more things. When I came here in 2015, at that time I was feeling like the groceries are very cheap here, as compared to back home. But in the last two or three years, basically starting off with this pandemic, especially the groceries, sometimes it's not affordable.I can give you the prices of that time, I was buying cucumber for just eighty cents, and now it's around $2 and $1.50. And sometimes I'm struggling to buy a lettuce.
+
+Nadia: Bread.
+
+Maliha: 
+
+Everything is expensive.
+
+Alizah: 
+
+So, it seems like we can look at the prices of petrol, also. The gas prices are going higher and higher every day. So, things, yeah. Now... When I came here, I feel like, "Oh, I'm very blessed that I came here to Canada," You know? And things, because I was very new. "Everything looks very perfect, and I like everything about Canada." But slowly and slowly, now, I am feeling like it's very challenging…. What we need to pay is very challenging. And especially when we don't have any relatives here. [Residents]`,
     },
     {
       title: 'Housing Crisis',
       text: 'The cost of housing is exorbitant and rising, leaving families with impossible choices. All participants, residents and workers alike, identified unaffordable housing as the main cause of food insecurity.',
       image: housingCrisis,
-      audio: 'placeholder.mp3',
-      transcript: 'Placeholder transcript',
+      audio: housingCrisisAudio,
+      transcript: `Bilal: 
+
+Housing is very important. Because I mean, I'm here 22 years. And I notice that Canada is going worse and worse.
+
+Sonia: 
+
+Oh yeah!
+
+Bilal: 
+
+Really it's ... I don't understand this country. First of all, the prices of housing, when we are beside the US. And when you buy a house in the US, $200,000. And here, any small room is one million.
+
+Sonia: 
+
+[Laughs.] One million!
+
+Bilal: 
+
+So, that's one thing that doesn't make sense to me, because the minimum wage is $15.50. And then renting a two bedroom is now 2,300 average, in our area. And I moved here almost six years ago. The price doubled exactly …
+
+Sonia: 
+
+Uuh-hmm.
+
+Bilal: 
+
+… for now. When you work at minimum wage, you make – you count it – you work 40 hours a week, you make around 2,200 a month. That's before tax deductions. So, I don't know, like, how can you live? Really!
+
+Farah: 
+
+Yeah!
+
+Bilal: 
+
+Life is becoming much worse. And to buy is becoming … It's impossible. And to go to government housing? I applied to government housing exactly from 2005. So, it's 17 years! 
+
+Ahmed: Same to me.
+
+Bilal: 
+
+They gave me two opportunities [for government housing]. I went there to see. People are using drugs in the front corridors.
+
+Sonia: 
+
+Oh gosh!
+
+Bilal: 
+
+And I said, "I don't want my kids to grow up around drugs, addicts." And it's terrible. This country is becoming much worse. I don't know how you can live. [Resident]
+
+Luann (Host): 
+
+You said you work part-time?
+
+Maliha: 
+
+I work part-time. But even if I work a full-time job, I won't be able to pay 1,700 rent for a one-bedroom apartment. Because the average market rent... Because I was just looking for an apartment. I just wanted to see how it goes. But it's too expensive nowadays. 1,700 is unaffordable! Like, I can't even think: how will I manage it? Because there's not just rent! It's groceries, plus the rent. Plus the hydro, plus... I don't have a car. I don't drive. Because owning a car is also not possible for me. Because I want to work as well, of course! My kids are growing up now. Now I can have some time for myself; do something for myself. But at the end of the day, if working a minimum wage job, it won't take me anywhere? I don't know. So… I'm just trying my best to make it work out. At least if I could get affordable housing, I could manage the other expenses.
+
+Luann (Host): 
+
+Then you feel like you could get ahead.
+
+Maliha: 
+
+Right. But I don't have that at the moment. So I hope I get lucky, and I get something for myself and kids. Yeah. Because I deal with a lot of anxiety. It's not easy. Because I just don't know how to deal with this. Yeah. [Resident]
+`,
     },
     {
       title: 'Childcare Demands',
       text: 'The time demands of parenting young children. When childcare is unaffordable and/or unavailable, it makes it difficult for women especially to provide for their families.',
       image: childcareDemands,
-      audio: 'placeholder.mp3',
-      transcript: 'Placeholder transcript',
+      audio: childcareDemandsAudio,
+      transcript: `Maliha: 
+
+I just don't understand, because I am in need, and I work part-time, too! But I am just struggling, because I have two kids with me, who are under 12 years of age, and then I have to work plus manage with my husband. But I am trying my best. And I have completed high school from here, but I couldn't study further, because I got married at the age of 19, and then I just got into this situation where I had two kids to raise, right? My younger son was only six weeks old when my husband left. And it was a very challenging time. And it's still challenging. Being a single parent is not easy, because you have to manage all the stress, anxiety. It's all on you, right? I mean, I want the best for my kids. I want my kids to study here, and do something good about themselves. I mean, I want to study further too, but I just don't know. How will I do it? Because they need my time right now as well. They are growing up. My older son is 11. The younger is 9 now. But, still, I need to be there for them. There's a lot of other things.  [Resident]`,
     },
     {
       title: 'Community Awareness',
       text: 'Both workers and residents revealed that a common challenge is that people are not aware of the resources and supports that are available to them.',
       image: communityAwareness,
-      audio: 'placeholder.mp3',
-      transcript: 'Placeholder transcript',
+      audio: communityAwarenessAudio,
+      transcript: `Luann (Host): 
+
+So are you using some government programs?
+
+Maliha: 
+
+Yes.
+
+Luann (Host): 
+
+And you mentioned that you live with your parents.
+
+Maliha: 
+
+Right.
+
+Luann (Host): 
+
+Are you finding any government programs or assistance programs that your parents have applied to, that are helpful for you or the family?
+
+Maliha: 
+
+No programs they applied for. What kind of programs?
+
+Luann (Host): There might be programs to do with healthcare, or perhaps a monthly subsidy based on your age. So are you aware of any programs?
+
+Maliha: 
+
+No. I am not aware of any programs that are happening, or they have. Yeah. [Resident]`,
     },
     {
       title: 'Access to Other Supports',
       text: 'While the presenting and immediate need may be food, there are often underlying needs that must be addressed. An important way to address food insecurity is to help people access a range of resources.',
       image: accessToOtherSupports,
-      audio: 'placeholder.mp3',
-      transcript: 'Placeholder transcript',
+      audio: accessToOtherSupportsAudio,
+      transcript: `Aida: 
+
+When a project comes to the end, this is when people start calling and saying their struggles. Like, “I'm a senior. I actually can afford food, but I need someone to come and cook for me.” And this is how we start connecting them with the Meals on Wheels program…. And I say, "Okay, do you have social worker? Maybe they will send you a PSW." Or some people say, "You know what, I have eight kids. That's why I applied for this program." And I'm starting, "Okay, are you connecting with your school? Because find out if they have breakfast, or snack program, and make sure your kids get into that program." So this is how we start connecting people with other agencies. Or someone says, "Can you find a job?” [Laughter] I have those kinds of requests.
+
+Omar (Host): 
+
+Yeah.
+
+Aida: 
+
+“Can you find the job for me? I see that you are very popular in the community and maybe you can find a job for me.” [Multiple voices laughing.] And I'm like, “Can you go to the employment center?” So this is how we are. We found out that actually food [insecurity] has really many causes, for struggling. So in connecting to those, if you know very well what kind of programs are available around, it's very good to connect people and say, “You can connect to them. And your issue would be solved. Quit struggling.” Right? [Worker]`,
     },
   ],
 };
